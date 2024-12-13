7751d83c2e66ff570c7340342eef7fce5baa015c
Feiyang Shen <2491435063@qq.com>
2024-12-13 15:13:39 +0800

    test

---
+++ src/main/java/nowcoder/BM/hash/BM51.java
@@ -0,0 +1,7 @@
+package nowcoder.BM.hash;
+
+public class BM51 {
+    public int MoreThanHalfNum_Solution (int[] numbers) {
+        return 0;
+    }
+}
--- src/main/java/nowcoder/BM/heap_stack_queue/BM49.java
+++ src/main/java/nowcoder/BM/heap_stack_queue/BM49.java
@@ -1,23 +1,42 @@
-package nowcoder.BM.heap_stack_queue;
-
-import java.util.Stack;
-
-public class BM49 {
-    Stack<Character> op = new Stack<>();
-    Stack<Integer> num = new Stack<>();
-    public int solve (String s) {
-        char[] chars = s.toCharArray();
-        for (char c : chars) {
-            if (c >= '0' && c <= '9') {
-
-            } else if (c == '+' || c == '-' || c == '*') {
-
-            } else if (c == '(') {
-
-            } else if (c == ')') {
-
-            }
-        }
-        return 0;
-    }
-}
+//package nowcoder.BM.heap_stack_queue;
+//
+//import java.util.Stack;
+//
+//public class BM49 {
+//    Stack<Integer> nums = new Stack<>();
+//    Stack<Character> ops = new Stack<>();
+//
+//    public int solve(String s) {
+//        char[] chars = s.toCharArray();
+//        int num = 0;
+//        for (int i = 0; i < chars.length; i++) {
+//            if (Character.isDigit(chars[i])) {
+//                num = num * 10 + chars[i];
+//                if (i != chars.length - 1) {
+//                    continue;
+//                }
+//            }
+//            nums.push(num);
+//            if (chars[i] == '+' || chars[i] == '-') {
+//                if(ops.peek() == '*') {
+//                    num = nums.pop() * nums.pop();
+//                    nums.push(num);
+//                }
+//            }
+//            if (chars[i] == '(') {
+//                ops.push(chars[i]);
+//            }
+//            if (chars[i] == '*') {
+//                ops.push(chars[i]);
+//            }
+//            if (chars[i] == ')') {
+//                while (!ops.isEmpty() && ops.peek() != '(') {
+//
+//                }
+//            }
+//        }
+//        // todo
+//        // 出栈，计算
+//        return num;
+//    }
+//}
